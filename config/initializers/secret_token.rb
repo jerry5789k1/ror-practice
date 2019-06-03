8fdc738b8148de699302648e112919d9e28ac11e
Qiu <chuijie.qiu@rtwpc10066.intra.rakuten.co.jp>
2019-06-03 14:20:25 +0800

    create new rails app, basic gemfile setting, sqlite3, pry, pry-byebug, nokogiri

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorPractice::Application.config.secret_token = 'a628b6d089935bd61ca30eff6af5dc2662ebc1671995610dcbbe538e29bec8e32f50142bd5ae0b5666a7dd55fb4d512c6a7ec37893bd1c9d327fece2a51c6bcd'
