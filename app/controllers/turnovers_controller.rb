8b370e938106a1bc623bf06dc4f9bb5efe9a6421
Qiu <chuijie.qiu@rtwpc10066.intra.rakuten.co.jp>
2019-06-05 11:14:15 +0800

    json data encode UTF-8

--- app/controllers/turnovers_controller.rb
+++ app/controllers/turnovers_controller.rb
@@ -5,12 +5,23 @@ class TurnoversController < ApplicationController
     def show
        WebCrawler.update_new_stocks_turnover_data_to_db
        @turnovers = Turnover.all
+       @turnovers = @turnovers.to_json.gsub(/\\u([0-9a-z]{4})/){|s| [$1.to_i(16)].pack("U")} #解碼中文
        render json: @turnovers
     end
 
     def filter
        @turnovers = Turnover.filter_turnover_record(params[:date],params[:code])
-       #@turnovers = @turnovers.to_json.gsub(/\\u([0-9a-z]{4})/){|s| [$1.to_i(16)].pack("U")} #解碼中文
+       @turnovers = @turnovers.to_json.gsub(/\\u([0-9a-z]{4})/){|s| [$1.to_i(16)].pack("U")} #解碼中文
        render json: @turnovers
     end
+
+    def get_all_dates 
+        @turnovers = Turnover.get_all_dates
+        render json: @turnovers
+    end
+
+    def get_all_codes
+        @turnovers = Turnover.get_all_stock_codes
+        render json: @turnovers
+    end 
 end
