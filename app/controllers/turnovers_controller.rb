5c57ba3f8dee3a4e92dedc757b23412e64fd4ce7
Qiu <chuijie.qiu@rtwpc10066.intra.rakuten.co.jp>
2019-06-05 10:34:01 +0800

    create filter action to filter turnover record

--- app/controllers/turnovers_controller.rb
+++ app/controllers/turnovers_controller.rb
@@ -5,6 +5,12 @@ class TurnoversController < ApplicationController
     def show
        WebCrawler.update_new_stocks_turnover_data_to_db
        @turnovers = Turnover.all
-       binding.pry
+       render json: @turnovers
+    end
+
+    def filter
+       @turnovers = Turnover.filter_turnover_record(params[:date],params[:code])
+       #@turnovers = @turnovers.to_json.gsub(/\\u([0-9a-z]{4})/){|s| [$1.to_i(16)].pack("U")} #解碼中文
+       render json: @turnovers
     end
 end
