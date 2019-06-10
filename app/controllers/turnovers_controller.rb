52c755ec98b19d6a505fe07a77d7eacdf70e6a82
Qiu <chuijie.qiu@rtwpc10066.intra.rakuten.co.jp>
2019-06-10 14:59:29 +0800

    refine API which is relative to filter feature

--- app/controllers/turnovers_controller.rb
+++ app/controllers/turnovers_controller.rb
@@ -9,10 +9,22 @@ class TurnoversController < ApplicationController
        render json: @turnovers
     end
 
-    def filter
+    def filter_by_all
        @turnovers = Turnover.filter_turnover_record(params[:date],params[:code])
        @turnovers = @turnovers.to_json.gsub(/\\u([0-9a-z]{4})/){|s| [$1.to_i(16)].pack("U")}
        render json: @turnovers
+    end
+
+    def filter_by_code
+        @turnovers = Turnover.filter_by_code(params[:code])
+        @turnovers = @turnovers.to_json.gsub(/\\u([0-9a-z]{4})/){|s| [$1.to_i(16)].pack("U")}
+        render json: @turnovers
+    end
+
+    def filter_by_date 
+        @turnovers = Turnover.filter_by_date(params[:date])
+        @turnovers = @turnovers.to_json.gsub(/\\u([0-9a-z]{4})/){|s| [$1.to_i(16)].pack("U")}
+        render json: @turnovers
     end
 
     def get_all_dates 
--- app/models/turnover.rb
+++ app/models/turnover.rb
@@ -1,20 +1,24 @@
 class Turnover < ActiveRecord::Base
   attr_accessible :stock_change, :stock_closing_price_today, :stock_closing_price_yesterday, :stock_code, :stock_company_url, :stock_day_high, :stock_day_low, :stock_name, :stock_opening_price, :stock_quote_change, :stock_volume
   def self.get_all_dates 
-    Turnover.select('DISTINCT created_at')
+      dates = Turnover.select("strftime('%d-%m-%Y', created_at) AS date").uniq.map do |date|
+          date
+      end
   end 
 
   def self.get_all_stock_codes
-      Turnover.select('stock_code').order(:stock_code)
+      Turnover.select('DISTINCT stock_code').order(:stock_code)
   end
 
-  def self.filter_turnover_record(date = nil, code = nil)
-    if code.nil?
-      Turnover.where(created_at: date.to_time.beginning_of_day..date.to_time.end_of_day)
-    elsif date.nil?
+  def self.filter_by_code(code)
       Turnover.where(stock_code: code)
-    else 
+  end 
+
+  def self.filter_by_date(date)
+      Turnover.where(created_at: date.to_time.beginning_of_day..date.to_time.end_of_day)
+  end 
+  
+  def self.filter_turnover_record(date, code) 
       Turnover.where(created_at: date.to_time.beginning_of_day..date.to_time.end_of_day, stock_code:code)
-    end
   end 
 end
