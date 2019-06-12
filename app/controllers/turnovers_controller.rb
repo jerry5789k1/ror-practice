69169be44ab6bdba9daf9bbfedb7c0b23315b2fc
Qiu <chuijie.qiu@rtwpc10066.intra.rakuten.co.jp>
2019-06-12 11:33:32 +0800

    code refactor combine filtering and sorting method into one method, sorting method sortingDataType use hashtable to assign value to query

--- app/controllers/turnovers_controller.rb
+++ app/controllers/turnovers_controller.rb
@@ -1,31 +1,46 @@
 class TurnoversController < ApplicationController
     def index
-    end
-
-    def show
-       WebCrawler.update_new_stocks_turnover_data_to_db
        @turnovers = Turnover.last(50)
-       @turnovers = @turnovers.to_json.gsub(/\\u([0-9a-z]{4})/){|s| [$1.to_i(16)].pack("U")}
-       render json: @turnovers
+       render :json => @turnovers
     end
 
-    def filter_by_all
-       @turnovers = Turnover.filter_turnover_record(params[:date],params[:code])
-       @turnovers = @turnovers.to_json.gsub(/\\u([0-9a-z]{4})/){|s| [$1.to_i(16)].pack("U")}
-       render json: @turnovers
-    end
+    def crawl 
+        WebCrawler.update_new_stocks_turnover_data_to_db
+    end 
 
-    def filter_by_code
-        @turnovers = Turnover.filter_by_code(params[:code])
-        @turnovers = @turnovers.to_json.gsub(/\\u([0-9a-z]{4})/){|s| [$1.to_i(16)].pack("U")}
-        render json: @turnovers
-    end
-
-    def filter_by_date 
-        @turnovers = Turnover.filter_by_date(params[:date])
-        @turnovers = @turnovers.to_json.gsub(/\\u([0-9a-z]{4})/){|s| [$1.to_i(16)].pack("U")}
-        render json: @turnovers
-    end
+    def filter_or_sort
+        sorting_data_type = {
+            'STOCK_CODE' => :stock_code,
+            'STOCK_NAME' => :stock_name,
+            'STOCK_OPENING_PRICE' => :stock_opening_price,
+            'STOCK_DAY_HIGH' => :stock_day_high,
+            'STOCK_DAY_LOW' => :stock_day_low,
+            'STOCK_CLOSING_PRICE_YES' => :stock_closing_price_yesterday,
+            'STOCK_CLOSING_PRICE_TODAY' => :stock_closing_price_today,
+            'STOCK_VOLUME' => :stock_volume,
+            'STOCK_CHANGE' => :stock_change,
+            'STOCK_QUOTE_CHANGE' => :stock_quote_change,
+        }
+        case params[:action_type]
+            when 'SORTING'
+                if params[:sort_type]
+                    @turnovers = Turnover.last(50).sort_by(&sorting_data_type[params[:sort_type]])
+                else
+                    @turnovers = Turnover.last(50)
+                end
+            else
+                if params[:date] && params[:code]
+                    @turnovers = Turnover.filter_turnover_record(params[:date],params[:code])
+                elsif params[:date]
+                    @turnovers = Turnover.filter_by_date(params[:date])
+                elsif params[:code]
+                    @turnovers = Turnover.filter_by_code(params[:code])
+                else
+                    @turnovers = Turnover.last(50) 
+                end
+        end
+        render :json => @turnovers
+    end 
 
     def get_all_dates 
         @turnovers = Turnover.get_all_dates
@@ -34,35 +49,6 @@ class TurnoversController < ApplicationController
 
     def get_all_codes
         @turnovers = Turnover.get_all_stock_codes
-        render json: @turnovers
-    end 
-
-    def sort
-        case params[:sort_type]
-            when 'STOCK_CODE'
-                @turnovers = Turnover.last(50).sort_by(&:stock_code)
-            when 'STOCK_NAME'
-                @turnovers = Turnover.last(50).sort_by(&:stock_name)
-            when 'STOCK_OPENING_PRICE'
-                @turnovers = Turnover.last(50).sort_by(&:stock_opening_price)
-            when 'STOCK_DAY_HIGH'
-                @turnovers = Turnover.last(50).sort_by(&:stock_day_high) 
-            when 'STOCK_DAY_LOW'
-                @turnovers = Turnover.last(50).sort_by(&:stock_day_low)
-            when 'STOCK_CLOSING_PRICE_YES'
-                @turnovers = Turnover.last(50).sort_by(&:stock_closing_price_yesterday)
-            when 'STOCK_CLOSING_PRICE_TODAY'
-                @turnovers = Turnover.last(50).sort_by(&:stock_closing_price_today)
-            when'STOCK_VOLUME'
-                @turnovers = Turnover.last(50).sort_by(&:stock_volume)
-            when'STOCK_CHANGE'
-                @turnovers = Turnover.last(50).sort_by(&:stock_change)
-            when'STOCK_QUOTE_CHANGE'
-                @turnovers = Turnover.last(50).sort_by(&:stock_quote_change)
-            when 'RANK'
-                @turnovers = Turnover.last(50)
-        end 
-        @turnovers = @turnovers.to_json.gsub(/\\u([0-9a-z]{4})/){|s| [$1.to_i(16)].pack("U")}
         render json: @turnovers
     end 
 end
